f9b9e91112f2e5f55738647b4fa703d7cba49075
Ahmad Haidari <ahmad.haidari@tum.de>
2022-04-11 14:41:28 +0200

    paper release

--- src/pi_min_curv.m
+++ src/pi_min_curv.m
@@ -1,7 +1,7 @@
 %% Initialization of the program
 
 % @author Ahmad Haidari
-% @date February 13, 2022
+% @date November 08, 2021
 
 % Cleanup
 clear
@@ -17,30 +17,56 @@ addpath("src/matlab")
 import gtsam.*
 import pi_racing.*
 
-% Settings
+t_total = tic;
+
+% Settings (adjustable by user)
 plot_loaded_dataset = true;
 is_flying_start = true;
 plot_zoomed = true;
 safety_dist = 1.0; % in meters
-boundary_cost_min = 2.0; % in meters
-boundary_cost_max = 3.0; % in meters
-bound_cost_max_perc = 0.99; 
 bound_cost_min_meter = 1.5;
 line_sample_rate = 1 / 0.5;
 
-%% Load dataset
-t_total = tic;
-t_loading = tic;
-
-% Track
+% Choose racetrack
 track_file = "berlin_2018.csv";
 %track_file = "modena_2019.csv";
 %track_file = "simple_racetrack.csv";
 %track_file = "handling_track.csv";
 %track_file = "rounded_rectangle.csv";
 
+% Factor cost values
+bounding_sigma = 1;
+min_angle_diff_sigma = 6e-3;
+if track_file == "modena_2019.csv"
+    min_angle_diff_sigma = 2e-3;
+end
+
+% Comparison of results is currently only available for the Berlin 2018 and
+% the Modena 2019 tracks
+compare_results = false;
+if track_file == "berlin_2018.csv"
+    comparison_file_1 = "res/min_curv_results_berlin.csv";
+    comparison_file_2 = "res/min_curv_iqp_results_berlin.csv";
+    compare_results = true;
+elseif track_file == "modena_2019.csv"
+    comparison_file_1 = "res/min_curv_results_modena.csv";
+    comparison_file_2 = "res/min_curv_iqp_results_modena.csv";
+    compare_results = true;
+end
+
+%% Load dataset
+t_loading = tic;
+
 % Load track
 [centerline, og_bounds_right, og_bounds_left] = load_dataset(track_file, line_sample_rate);
+
+t_loading = toc(t_loading);
+
+disp("Finished loading racetrack");
+
+%% Pre-processing
+
+t_pre_processing = tic;
 
 % Compute angles between points
 angles = zeros(length(centerline), 1);
@@ -50,15 +76,20 @@ for i = 1:length(centerline)
     angles(i) = get_angle(centerline(i,:), centerline(next,:), centerline(after_next,:));
 end
 
-% Compute dynamic cost boundary depending on angle
-angles_n = rescale(angles, 0, bound_cost_max_perc);
-dyn_cost_boundary = vecnorm((og_bounds_right - og_bounds_left)')' * 0.5 .* angles_n;
-dyn_cost_boundary = max(bound_cost_min_meter, dyn_cost_boundary);
+% Compute individual cost boundary depending on angle
+%angles_n = angles ./ max(angles);
+angles_n = rescale(angles, 0, 1); 
+ind_cost_boundary = vecnorm((og_bounds_right - og_bounds_left)')' * 0.5 .* angles_n;
+ind_cost_boundary = max(bound_cost_min_meter, ind_cost_boundary);
 
 % Get track boundaries
-[bound_cost_start_left, bound_cost_start_right] = eps_dist_boundaries(og_bounds_left, og_bounds_right, dyn_cost_boundary);
+[bound_cost_start_left, bound_cost_start_right] = eps_dist_boundaries(og_bounds_left, og_bounds_right, ind_cost_boundary);
+
+t_pre_processing = toc(t_pre_processing);
+
 [bound_safety_left, bound_safety_right] = eps_dist_boundaries(og_bounds_left, og_bounds_right, safety_dist);
 
+% Plot the racetrack with cost boundaries
 if plot_loaded_dataset
     figure(1);
     clf;
@@ -72,49 +103,40 @@ if plot_loaded_dataset
     plot(bound_cost_start_right(:,1), bound_cost_start_right(:,2), "g-", "LineWidth", 2.0);
     plot(og_bounds_left(:,1), og_bounds_left(:,2), "k-", "LineWidth", 2.0);
     plot(bound_cost_start_left(:,1), bound_cost_start_left(:,2), "g-", "LineWidth", 2.0);
-    legend("Centerline", "Track Boundary", "Cost Boundaries");
+    legend("Centerline", "Track Boundary", "Cost Boundary");
     title("Racetrack with Cost Boundaries");
     xlabel("x [m]");
     ylabel("y [m]");
 end
 
-disp("Finished loading racetrack");
+disp("Finished Pre-Processing")
 
-%% Factor Graph Settings
+%% Factor Graph 
+
+t_init_graph = tic;
 
 % Variable nodes
 max_step = length(centerline);
 
 % Bounding factor parameters 1
-bounding_sigma = 1;
 bounding_model = noiseModel.Gaussian.Covariance(eye(2) * bounding_sigma);
 
 % Minimum angle factor parameters 
-% - 8e-2 for 1/4 sample rate
-% - 6e-3 for 1/2 sample rate
-% - 1.6e-3 for 1/1 sample rate
-min_dist_sigma = 5e-1;
-min_dist_diff_model = noiseModel.Gaussian.Covariance(eye(2) * min_dist_sigma);
+min_angle_diff_model = noiseModel.Gaussian.Covariance(eye(2) * min_angle_diff_sigma);
 
 % Prior to start/goal
 fixed_point_model = noiseModel.Gaussian.Covariance(eye(2) * 0.0001);
 
 disp("Finished applying settings");
 
-t_loading = toc(t_loading);
-
-%% Init trajectory computation
-
-t_init_graph = tic;
-
-[init_values, graph] = build_graph_min_dist(...
+[init_values, graph] = build_graph(...
     centerline,...
     bound_cost_start_left,...
     bound_cost_start_right,...
     is_flying_start,...
     fixed_point_model,...
     bounding_model,...
-    min_dist_diff_model...
+    min_angle_diff_model...
 );
 
 t_init_graph = toc(t_init_graph);
@@ -158,28 +180,37 @@ disp("Finished post processing");
 t_plotting = tic;
 
 % Load results from MinCurv paper
-result_other = readtable("res/min_curv_paper_results.csv");
-result_pos_other = [result_other{:, "x_m"}, result_other{:, "y_m"}];
-%indices = round(linspace(1, length(other_results_traj), length(centerline)));
-%other_results_traj = other_results_traj(indices, :);
-
-% Upsample the resulting trajectory to match the reference trajectory
-%sample_rate = length(other_results_traj) / length(result_pos);
-%result_pos = resample(result_pos, length(other_results_traj), length(result_pos));
+if compare_results
+    result_other = readtable(comparison_file_1);
+    result_pos_other = [result_other{:, "x_m"}, result_other{:, "y_m"}];
+    writematrix(result_pos_other, "output/optimization_based.csv");
+    
+    result_other = readtable(comparison_file_2);
+    result_pos_other_iqp = [result_other{:, "x_m"}, result_other{:, "y_m"}];
+    writematrix(result_pos_other_iqp, "output/optimization_based_iqp.csv");
+end
 
 % Save results to file
 disp("Exporting results");
 writematrix(result_pos, "output/pi_results.csv");
 writematrix(centerline, "output/centerline.csv");
-writematrix(result_pos_other, "output/optimization_based.csv");
 
 % Compute curvature of both trajectories
 [total_curvature, curvatures] = compute_curvature(result_pos, true);
-[total_curvature_other, curvatures_other] = compute_curvature(result_pos_other, true);
+if compare_results
+    [total_curvature_other, curvatures_other] = compute_curvature(result_pos_other, true);
+else
+    total_curvature_other = -1;
+    curvatures_other = -1;
+end
 
 % Compute the total distance travelled of both trajectories
-total_distance = compute_distance_travelled(result_pos);
-total_distance_other = compute_distance_travelled(result_pos_other);
+total_distance = compute_distance_travelled(result_pos, true);
+if compare_results
+    total_distance_other = compute_distance_travelled(result_pos_other, true);
+else
+    total_distance_other = -1;
+end
 
 %% Plot results
 
@@ -193,13 +224,20 @@ hold on;
 title("Optimized results - Minimum Curvature");
 xlabel("x [m]");
 ylabel("y [m]");
-plot(centerline(:, 1), centerline(:, 2), "b-", "LineWidth", 2);
-plot(result_pos_other(:, 1), result_pos_other(:, 2), "g-", "LineWidth", 2);
+%plot(centerline(:, 1), centerline(:, 2), "b-", "LineWidth", 2);
+if compare_results
+    plot(result_pos_other(:, 1), result_pos_other(:, 2), "c-", "LineWidth", 2);
+    plot(result_pos_other_iqp(:, 1), result_pos_other_iqp(:, 2), "b-", "LineWidth", 2);
+end
 plot(result_pos(:, 1), result_pos(:, 2), "r-", "LineWidth", 2);
 plot(og_bounds_right(:, 1), og_bounds_right(:, 2), "k-", "LineWidth", 2);
 plot(result_pos(states_outbounds, 1), result_pos(states_outbounds, 2), "mo", "LineWidth", 2);
 plot(og_bounds_left(:, 1), og_bounds_left(:, 2), "k-", "LineWidth", 2);
-legend("Centerline", "Comparison Trajectory", "Optimized Trajectory", "Track Boundaries");
+if compare_results
+    legend("HWH+20", "HWH+20 - iterative QP", "Probabilistic Inference", "Track Boundaries");
+else
+    legend("Centerline", "Optimized Trajectory", "Track Boundaries");
+end
 
 t_plotting = toc(t_plotting);
 t_total = toc(t_total);
@@ -212,8 +250,8 @@ fprintf("Number of iterations: %d\n", optimizer.iterations())
 
 fprintf("Out of bounds states: %d\n", sum(states_outbounds));
 
-fprintf("Timings:\n- %.3fs - Loading Time\n- %.3fs - Init Graph Time\n- %.3fs - Optimization Time\n- %.3fs - Checking Trajectory Time\n- %.3fs - Plotting Time\n- %.3fs - Total Time\n",...
-    t_loading, t_init_graph, t_optimization, t_checking, t_plotting, t_total);
+fprintf("Timings:\n- %.3fs - Loading Time\n- %.3fs - Pre-Processing Time\n- %.3fs - Init Graph Time\n- %.3fs - Optimization Time\n- %.3fs - Checking Trajectory Time\n- %.3fs - Plotting Time\n- %.3fs - Total Time\n",...
+    t_loading, t_pre_processing,t_init_graph, t_optimization, t_checking, t_plotting, t_total);
 
 fprintf("Error: %.3f (Centerline) --> %.3f (Optimized)\n", graph.error(init_values), graph.error(results));
 
